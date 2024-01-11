4a7abfbbf40af179a4826a035b780eda24f5ddeb
Nat <64110681+vidmontng@users.noreply.github.com>
2024-01-10 21:25:23 -0500

    Added quotes
    Added 9 quotes. According to the project instructions, added 3 quotes with the "year" property, one with "citation" and one with both "year" and "citation".

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,7 +10,50 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: "Keep smiling, because life is a beautiful thing and there’s so much to smile about.",
+    source: "- Marilyn Monroe",
+  },
+  {
+    quote: "The beautiful thing about learning is nobody can take it away from you.",
+    source: "- B.B. King",
+    year: "September 1996"
+  },
+  {
+    quote: "May the Force be with you",
+    source: "- Obi-Wan Kenobi",
+    citation: "Star Wars, 1977"
+  },
+  {
+    quote: "The future belongs to those who believe in the beauty of their dreams.",
+    source: "- Eleanor Roosevelt"
+  },
+  {
+    quote: "After all, tomorrow is another day!",
+    source: "- Scarlett",
+    citation: "Gone with the Wind",
+    year: "1939"
+  },
+  {
+    quote: "Always forgive your enemies; nothing annoys them so much.",
+    source: "- Oscar Wilde",
+    year: "June 1954"
+  },
+  {
+    quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+    source:"- Oprah Winfrey"
+  },
+  {
+    quote: "Well done is better than well said",
+    source: "- Benjamin Franklin"
+  },
+  {
+    quote: "Not in doing what you like but in liking what you do is the secret of happiness.",
+    source: "- J.M. Barrie",
+    year: "August 1933"
+  }  
+];
 
 
 /***
